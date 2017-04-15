c50671f5e324ce25d2e3e20d1b26eb22980b7003
Chao-Wei, Peng <alanwei1202@gmail.com>
2017-04-16 00:27:39 +0800

    add common php in views

---
+++ application/views/common/footer.php
@@ -0,0 +1,46 @@
+<!--footer-->
+<div class="footer">
+    <div class="container">
+        <div class="col-md-3 footer-left wow fadeInLeft animated" data-wow-delay=".5s">
+            <h4>Snippet</h4>
+            <p>Sed diam nonummy nibh eu ismod tincidunt ut laoreetd.sed diam nonummy .</p>
+        </div>
+        <div class="col-md-3 footer-left wow fadeInLeft animated" data-wow-delay=".5s">
+            <h4>Features</h4>
+            <ul>
+                <li><a href="singlepage.html">Sed diam eu</a></li>
+                <li><a href="singlepage.html">Ismod tincidunt </a></li>
+                <li><a href="singlepage.html">Tincidunt ut </a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 footer-left wow fadeInRight animated" data-wow-delay=".5s">
+            <h4>Quick Links</h4>
+            <ul>
+                <li><a href="singlepage.html">Ismod tincidunt </a></li>
+                <li><a href="singlepage.html">Tincidunt ut </a></li>
+                <li><a href="singlepage.html">Sed diam eu</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 soci wow fadeInRight animated" data-wow-delay=".5s">
+            <h4>Follow Us</h4>
+            <ul>
+                <li><a href="#"><i class="f-1"> </i></a></li>
+                <li><a href="#"><i class="t-1"> </i></a></li>
+                <li><a href="#"><i class="g-1"> </i></a></li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
+        <div class="copy animated wow fadeInUp animated animated" data-wow-duration="1200ms" data-wow-delay="500ms">
+            <p>© 2016 Mentors . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+        </div>
+    </div>
+</diV>
+<!--footer-->
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: snailpjw
+ * Date: 2017/4/16
+ * Time: 上午 12:10
+ */
---
+++ application/views/common/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mentors a Education Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+    <!-- Custom Theme files -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Mentors Responsive Web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible Web template,
+Smartphone Compatible Web template, free Webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola Web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //Custom Theme files -->
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+    <!-- js -->
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.js"> </script>
+    <!-- //js -->
+    <!--fonts-->
+    <link href='//fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+    <!--/fonts-->
+    <!--JS for animate-->
+    <link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+    <!--//end-animate-->
+
+</head>
+<body>
+<!--banner-->
+<div class="banner">
+    <!--header-->
+    <div class="header">
+        <nav class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"> </span>
+                    <span class="icon-bar"> </span>
+                    <span class="icon-bar"> </span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right wow fadeInLeft animated" data-wow-delay=".5s">
+                    <li><a href="index.html" class="active">Home</a></li>
+                    <li><a href="admission.html" >Admission</a></li>
+                    <li><a href="staff.html" >Staff</a></li>
+                    <li><a href="shortcodes.html" >Short Codes</a></li>
+                    <li><a href="contact.html" >Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="logo">
+            <a class="navbar-brand" href="index.html">Mentors</a>
+        </div>
+
+        <div class="search-bar wow fadeInRight animated" data-wow-delay=".5s">
+            <form action="#" method="post">
+                <input type="text" name="search" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+                <input type="submit" value="">
+            </form>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+    <!--//header-->
+/**
+ * Created by PhpStorm.
+ * User: snailpjw
+ * Date: 2017/4/16
+ * Time: 上午 12:11
+ */
